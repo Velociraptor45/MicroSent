317e6973ba87f8771ef8e6dbf6b073a209c04186
David Krammer <dk@popkultur.bayern>
2019-10-11 15:48:49 +0200

    Implement analysis of xlsx file

---
+++ ExcelSheetAnalysis/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.IO;
+
+namespace ExcelSheetAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string FilePath = @"data\emojis.xlsx";
+
+            Program p = new Program();
+            p.readFile(FilePath);
+        }
+
+        private Dictionary<char[], int> positiveEmojis = new Dictionary<char[], int>();
+        private Dictionary<char[], int> negativeEmojis = new Dictionary<char[], int>();
+        private Dictionary<string, int> positiveSmileys = new Dictionary<string, int>();
+        private Dictionary<string, int> negativeSmileys = new Dictionary<string, int>();
+
+        private const int PositiveSmileyColumnNumber = 1;
+        private const int NegativeSmileyColumnNumber = 2;
+        private const int PositiveEmojiColumnNumber = 3;
+        private const int NegativeEmojiColumnNumber = 4;
+
+        private const string SheetName = "antw1";
+
+        private void readFile(string filepath)
+        {
+            // src: https://stackoverflow.com/questions/16079956/does-npoi-have-support-to-xlsx-format
+            XSSFWorkbook workbook;
+            using(FileStream stream = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+            {
+                workbook = new XSSFWorkbook(stream);
+            }
+
+            ISheet sheet = workbook.GetSheet(SheetName);
+
+            IRow row;
+            int rowNumber = 2;
+            while((row = sheet.GetRow(rowNumber)) != null)
+            {
+                cutEmojisFromCell(row.GetCell(PositiveEmojiColumnNumber), positiveEmojis);
+                cutEmojisFromCell(row.GetCell(NegativeEmojiColumnNumber), negativeEmojis);
+                cutSmileysFromCell(row.GetCell(PositiveSmileyColumnNumber), positiveSmileys);
+                cutSmileysFromCell(row.GetCell(NegativeSmileyColumnNumber), negativeSmileys);
+                rowNumber++;
+            }
+        }
+
+        private void cutEmojisFromCell(ICell cell, Dictionary<char[], int> dictionary)
+        {
+            string[] lines = cell.StringCellValue.Trim().Split('\n');
+
+            foreach(var line in lines)
+            {
+                addResultToDict(dictionary, line.ToCharArray());
+            }
+        }
+
+        private void cutSmileysFromCell(ICell cell, Dictionary<string, int> dictionary)
+        {
+            string[] lines = cell.StringCellValue.Trim().Split('\n');
+
+            foreach (var line in lines)
+            {
+                addResultToDict(dictionary, line);
+            }
+        }
+
+        private void addResultToDict(Dictionary<string, int> dictionary, string result)
+        {
+            var keys = dictionary.Keys.Where(k => k == result).ToList();
+
+            if (keys.Count == 0)
+            {
+                dictionary.Add(result, 0);
+            }
+            else if (keys.Count == 1)
+            {
+                dictionary[keys.First()]++;
+            }
+            else
+            {
+                // shoudn't happen
+                return;
+            }
+        }
+
+        private void addResultToDict(Dictionary<char[], int> dictionary, char[] result)
+        {
+            if (result.Length != 2)
+                return;
+
+            var keys = dictionary.Keys.Where(k => k[0] == result[0] && k[1] == result[1]).ToList();
+
+            if(keys.Count == 0)
+            {
+                dictionary.Add(result, 0);
+            }
+            else if(keys.Count == 1)
+            {
+                dictionary[keys.First()]++;
+            }
+            else
+            {
+                // shoudn't happen
+                return;
+            }
+        }
+    }
+}
