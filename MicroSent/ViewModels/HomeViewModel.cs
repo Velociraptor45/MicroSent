f0aeb663306e79150a39de28a6563596ee914a30
David Krammer <dk@popkultur.bayern>
2019-11-16 12:25:44 +0100

    Implement HomeViewModel as interface to front end

---
+++ MicroSent/ViewModels/HomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MicroSent.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string accountName { get; }
+        public List<Tuple<string, int>> linkRatings { get; }
+        public List<Tuple<string, int>> accountRatings { get; }
+
+        public HomeViewModel(string accountName, List<Tuple<string, int>> linkRatings,
+            List<Tuple<string, int>> accountRatings)
+        {
+            this.accountName = accountName;
+            this.linkRatings = linkRatings;
+            this.accountRatings = accountRatings;
+        }
+    }
+}
